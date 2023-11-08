7fb0abf854a20174ee9e7439e1aced5eb6d97bcd
Dionel22 <diondiaz293@gmail.com>
2023-11-07 21:53:55 -0300

    Components

---
+++ src/Components/Card/Card.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import style from './Card.module.css'
+import { Link } from "react-router-dom"
+
+export default function Card(props) {
+    return (
+        <>
+          {props.title ? <Link to={`/detail/${props?.id}`} className={style.link}>
+            <div key={props?.id} className={style.div}>
+    
+              <img className={style.image} src={props.image} alt={props.title} />
+    
+              <h2 className={style.name} >{props.title}</h2>
+                <h1 className={style.diet}>Types Of Diets</h1>
+                <div className={style.listcard }>
+                {props.diets?.map((diet, index) => {
+                  return <span key={index} className={style.types}>{diet.name}</span>
+                })}
+             </div>
+            </div>
+    
+          </Link> :
+            <div>
+              <h2 className={style.msg}>{props.msg}. </h2>
+            </div>}
+        </>
+  )
+}
---
+++ src/Components/Cards/Cards.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import style from './Cards.module.css'
+import Card from "../Card/Card"
+
+export default function Cards(props) {
+  return (
+    <div className={style.card_list}>
+    {props.allFoods.msg ? <Card msg={props.allFoods.msg} /> : props.allFoods.map((food) => {
+      return <Card
+        key={food.id}
+        id={food.id}
+        title={food.title}
+        image={food.image}
+        diets={food.diets}
+      />
+    })}
+  </div>
+  )
+}
