ec2312c64d227892b17dbb67e33db5b353c800b0
Dionel22 <diondiaz293@gmail.com>
2023-11-07 21:16:54 -0300

    Views

---
+++ src/View/DetailPage/DetailPage.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import style from './DetailPage.module.css'
+
+export default function DetailPage() {
+  return (
+    <div>DetailPage</div>
+  )
+}
---
+++ src/View/FormPage/FormPage.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import style from './FormPage.module.css'
+
+export default function FormPage() {
+  return (
+    <div>FormPage</div>
+  )
+}
