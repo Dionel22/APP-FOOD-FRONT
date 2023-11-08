29043be3b0df9fde2bd31d53f4a6a65f1b02b52a
Dionel22 <diondiaz293@gmail.com>
2023-11-07 22:00:27 -0300

    Components

---
+++ src/Components/Paginado/Paginado.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import style from './Paginado.module.css'
+
+export default function Paginado({ pagina, allFoods, currentPagina, handlesPag }) {
+    let pageNumberArray = [];
+    let currentPageNumber = pagina;
+    const division = Math.ceil(allFoods / currentPagina)
+    for (let i = 1; i <= division; i++) {
+      pageNumberArray.push(i)
+    }
+  
+    const handlesButton = (value) => {
+      handlesPag(value)
+    }
+  
+    const handlesButtonNext = () => {
+      if (currentPageNumber < division) {
+        currentPageNumber++;
+        handlesPag(currentPageNumber);
+      }
+    }
+    const handlesButtonPrev = () => {
+      if (currentPageNumber > 1) {
+        currentPageNumber--;
+        handlesPag(currentPageNumber);
+      }
+    }
+  
+    return (
+      <div >
+        <button className={style.boton} onClick={handlesButtonPrev}>Prev</button>
+        {pageNumberArray?.map((e, i) => {
+          return <button
+            key={i}
+            className={e === currentPageNumber ? style.boton : style.ahora}
+            onClick={() => handlesButton(e)}>{e}</button>
+        })}
+        <button className={style.boton} onClick={handlesButtonNext}>Next</button>
+      </div>
+    )
+}
---
+++ src/Components/SearchBar/SearchBar.jsx
@@ -0,0 +1,95 @@
+import React from 'react'
+import style from './SearchBar.module.css'
+import { useState, useEffect } from 'react'
+import { useDispatch } from "react-redux"
+import { filteredByDiets, getByName } from '../../Redux/Actions/action'
+
+export default function SearchBar() {
+    const [name, setName] = useState("");
+    const dispatch = useDispatch();
+  
+      const [filtro, setFiltro] = useState({
+          title: "title",
+        titles: "",
+        api: "",
+        diet: "diet",
+        diets: ""
+      });
+  console.log("filtros", filtro);
+      const handleName = (e) => {
+          const { value } = e.target;
+          setFiltro({
+              ...filtro,
+              titles: value,
+          });
+          //setPage(1);
+      };
+  
+      //diet
+      const handlesOrdenByDiets = (event) => {
+          const { value } = event.target;
+          setFiltro({
+              ...filtro,
+              diets: value
+          });
+          //setPage(1);
+      };
+  
+  
+      useEffect(() => {
+          dispatch(filteredByDiets(filtro));
+      }, [filtro, dispatch]);
+  
+      const handleReset = () => {
+          dispatch(getAllBooks());
+          setFiltro({
+        title: "title",
+        titles: "",
+        api: "",
+        diet: "diet",
+        diets: ""
+          });
+          //setPage(1);
+      };
+  console.log("propss", props);
+    const handlesSubmit = (event) => {
+      event.preventDefault()
+      dispatch(getByName(name))
+      setName("")
+      props.pagina(1)
+    }
+  
+    return (
+      <div className={style.div}>
+        <input
+          className={style.input}
+          type="text"
+          placeholder="busqueda..."
+          value={name.trim()}
+          onChange={handleName}
+        />
+        <button className={style.boton} onClick={handlesSubmit}>Buscar</button>
+  
+  
+  
+          {/*ORDEN POR DIETA*/}
+          <select className={style.select} defaultValue='msg' onChange={handlesOrdenByDiets}>
+            <option value="msg" disabled>Type of Diets</option>
+            <option value="gluten free">gluten free</option>
+            <option value="dairy free">dairy free</option>
+            <option value="lacto ovo vegetarian">lacto ovo vegetarian</option>
+            <option value="vegan">vegan</option>
+            <option value="paleolithic">paleolithic</option>
+            <option value="primal">primal</option>
+            <option value="whole 30">whole 30</option>
+            <option value="pescatarian">pescatarian</option>
+            <option value="ketogenic">ketogenicl</option>
+            <option value="fodmap friendly">fodmap friendly</option>
+          </select>
+  
+   
+          {/*RESETEA*/}
+          <button className={style.select} onClick={handleReset}>Reset</button>
+      </div>
+    )
+}
